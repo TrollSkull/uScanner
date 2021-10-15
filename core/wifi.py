48aff641b89df8333ce468d7697b8d48f388e95e
TrollSkull <leanahuelmiranda@gmail.com>
2021-10-15 22:08:46 +0200

    Update wifi.py

--- core/wifi.py
+++ core/wifi.py
@@ -1,12 +1,12 @@
 from socket import gethostbyname, create_connection, error
 
 def writeFile_1():
-    file = open(".wifi_connection.sh", "w")
+    file = open("wifi_connection.sh", "w")
     file.write("""#!/bin/bash
 export wifi='1'""")
 
 def writeFile_0():
-    file = open(".wifi_connection.sh", "w")
+    file = open("wifi_connection.sh", "w")
     file.write("""#!/bin/bash
 export wifi='0'""")
 
@@ -15,9 +15,9 @@ def checkWifi():
         gethostbyname("google.com")
         conexion = create_connection(("google.com", 80), 1)
         conexion.close()
-        writeFile_1
+        writeFile_1()
 
     except error:
-        writeFile_0
+        writeFile_0()
 
-checkWifi
+checkWifi()
